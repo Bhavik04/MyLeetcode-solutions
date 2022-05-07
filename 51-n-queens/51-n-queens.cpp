5757e4d3c2bcbe24d6aa3edd2328c06e3e6d0134
Bhavik Choudhary <69357691+Bhavik04@users.noreply.github.com>
2022-05-07 14:25:27 +0530

    Time: 6 ms (77.70%), Space: 7.3 MB (69.36%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    bool safe(int row, int col, int n, vector<string> v){
+   /* bool safe(int row, int col, int n, vector<string> v){
         int r=row,c=col;
         
         while(col>=0){
@@ -23,15 +23,21 @@ public:
             col--;
         }
         return true;
-    }
+    } */
     
-    void solve(vector<vector<string>>& ans, vector<string> &v,int col, int n ){
+    void solve(vector<vector<string>>& ans, vector<string> &v,int col, int n , vector<int>& lrow, vector<int>& udiagonal, vector<int>& ldiagonal ){
         if(col==n){ans.push_back(v);return;}
         
         for(int row=0;row<n;row++){
-            if(safe(row,col,n,v)){
+            if(lrow[row]==0 && ldiagonal[row+col]==0 && udiagonal[(n-1)+(row-col)]==0){
                 v[row][col]='Q';
-                solve(ans,v,col+1,n);
+                lrow[row]=1;
+                ldiagonal[row+col]=1;
+                udiagonal[(n-1)+(row-col)]=1;
+                solve(ans,v,col+1,n,lrow,udiagonal,ldiagonal);
+                 lrow[row]=0;
+                ldiagonal[row+col]=0;
+                udiagonal[(n-1)+(row-col)]=0;
                 v[row][col]='.';
                 
             }
@@ -45,9 +51,11 @@ public:
         for(int i=0;i<n;i++){
             v[i]=s;
         }
+        vector<int> lrow(n),udiagonal(2*n-1),ldiagonal(2*n-1);
+        
         //ans.push_back(v);
        // cout<<ans[1][1];
-        solve(ans,v,0,n);
+        solve(ans,v,0,n,lrow,udiagonal,ldiagonal);
         return ans;
     }
 };
