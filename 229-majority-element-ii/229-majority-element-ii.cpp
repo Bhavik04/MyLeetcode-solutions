7e72e7f3c1c12a5ee823007296fa70d9cbcc20f9
Bhavik Choudhary <69357691+Bhavik04@users.noreply.github.com>
2022-05-30 00:57:10 +0530

    Time: 20 ms (41.79%), Space: 15.7 MB (96.28%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,14 +1,44 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int> umap;
-        for(auto &x:nums){
-            umap[x]++;
-        }
+        int cnt1=0,cnt2=0;
+        int num1,num2;
         vector<int> ans;
-        for(auto &x: umap){
-            if(x.second>nums.size()/3) ans.push_back(x.first);
+        
+        
+        //finds 2 possible majority elements
+        for(auto &x:nums){
+            if(num1==x)cnt1++;
+            else if(num2==x)cnt2++;
+            else if(cnt1==0){
+                num1=x;cnt1=1;
+            }
+            else if(cnt2==0){
+                num2=x;cnt2=1;
+            }
+           
+            else{
+                cnt1--;cnt2--;
+            }
         }
+        
+        
+        
+        //to check frequency of those 2 elemets
+        cnt1=cnt2=0;
+        for(auto &x:nums){
+            if(x==num1)cnt1++;
+            else if(x==num2)cnt2++;
+            
+            
+        }
+        
+        //checks if they satisfy the condition
+        if(cnt1>nums.size()/3)ans.push_back(num1);
+        if(cnt2>nums.size()/3)ans.push_back(num2);
+            cout<<num1<<num2;
+            
         return ans;
+        
     }
 };
