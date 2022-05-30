501744ab4842fe0302fba349297d1b1f7f2d79ff
Bhavik Choudhary <69357691+Bhavik04@users.noreply.github.com>
2022-05-31 00:33:06 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (74.11%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
-    int solve(int i,int j,int m, int n,vector<vector<int>>& dp){
-        if(i==m-1 && j==n-1)return 1;
-        if(i>=m || j>=n) return 0;
-        
-        if(dp[i][j]!=-1)return dp[i][j];
-        else return dp[i][j]=solve(i+1,j,m,n,dp)+solve(i,j+1,m,n,dp);
-    }
+    
     
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int> (n,-1));
-        int ans=solve(0,0,m,n,dp);
+        int k=m+n-2;
+        int j=m-1;
         
+        double ans=1;
+        for(int i=1;i<=j;i++){
+            ans=ans*(k-j+i)/i;
+        }
         
-        
-        return ans;
+        return (int)ans;
     }
 };
