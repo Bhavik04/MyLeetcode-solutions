06b4286cfe1c57f340c98695b263367958c823f4
Bhavik Choudhary <69357691+Bhavik04@users.noreply.github.com>
2022-06-12 00:42:35 +0530

    Time: 912 ms (53.51%), Space: 210.1 MB (27.42%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
-    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        
-        vector<int> adj[n];
-        
-        vector<int> vis(n+1,0);
-        for(auto &x: edges){
-            adj[x[0]].push_back(x[1]);
+    
+    bool dfs(int source, int destination, vector<int> adj[], vector<int>& vis){
+            if(source==destination)return true;
+            //if(vis[source])return false;
+            vis[source]=1;
             
-            adj[x[1]].push_back(x[0]);
-        }
+            for(auto &x: adj[source]){
+                if(!vis[x]){
+                    //if(x==destination)return true;
+                    
+                    if(dfs(x,destination,adj,vis))return true;
+                }
+            }
+           
         
+        return false;
+    }
+    
+    bool bfs(int source, int destination, vector<int> adj[], vector<int> vis){
         queue<int> q;
         q.push(source);
         vis[source]=1;
@@ -26,5 +34,23 @@ public:
             }
         }
         return false;
+    }
+    
+    
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        
+        vector<int> adj[n];
+        
+        vector<int> vis(n+1,0);
+        for(auto &x: edges){
+            adj[x[0]].push_back(x[1]);
+            
+            adj[x[1]].push_back(x[0]);
+        }
+        
+        bool ans=dfs(source,destination,adj,vis);
+        
+        
+        return ans;
     }
 };
