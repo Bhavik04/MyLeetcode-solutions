a8c170cebb6271d84e708d1e3eee709800fffae2
Bhavik Choudhary <69357691+Bhavik04@users.noreply.github.com>
2022-06-25 16:36:25 +0530

    Time: 375 ms (20.19%), Space: 83 MB (77.44%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& A) {
+        
+        
+        //put each element in its right place
         int n=A.size();
         for(int i = 0; i < n; ++ i)
             while(A[i] > 0 && A[i] <= n && A[A[i] - 1] != A[i])
                 swap(A[i], A[A[i] - 1]);
         
+        
+        //find first element not on its right place 
         for(int i = 0; i < n; ++ i)
             if(A[i] != i + 1)
                 return i + 1;
         
+        
+        //if every element is at its place ans is arr size+1
         return n + 1;
        
     }
