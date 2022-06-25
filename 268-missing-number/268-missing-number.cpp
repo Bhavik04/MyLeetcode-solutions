32b877a7fcb8411074f2172b1f109c2e2d7d68a6
Bhavik Choudhary <69357691+Bhavik04@users.noreply.github.com>
2022-06-25 17:32:37 +0530

    Time: 27 ms (58.92%), Space: 17.9 MB (93.45%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-       for(int i=0;i<nums.size();i++){
-           while(nums[i]!=nums.size() &&  nums[i]!=i)
-               swap(nums[i],nums[nums[i]]);
-       }
-        
-        for(int i=0;i<nums.size();i++){
-            if(i!=nums[i])return i;
+      
+        //method 1 using XOR
+        int ans=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            ans=ans^i^nums[i];    //uses fact that a^b^b=a   element that only comes ones, stays.
         }
-        return nums.size();
+        return ans^n;
+        
+        
+        /*
+        
+        //method 2 putting element at its place
+            for(int i=0;i<nums.size();i++){
+               while(nums[i]!=nums.size() &&  nums[i]!=i)
+                   swap(nums[i],nums[nums[i]]);
+           }
+
+            for(int i=0;i<nums.size();i++){
+                if(i!=nums[i])return i;
+            }
+            return nums.size();
+        */
     }
 };
